72e901dca8a10c5d5eea1363210babc0ab825dc1
hjf <947512302@qq.com>
2022-01-08 18:44:48 +0800

    use relative paths

--- js/Carousel.js
+++ js/Carousel.js
@@ -4,7 +4,7 @@
  * @LastEditTime: 2021-12-30 11:38:32
  * @LastEditors: your name
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: /MusicBlog/js/Carousel.js
+ * @FilePath: /MusicBlog../../js/Carousel.js
  */
 /**
  * @description: 轮播图
